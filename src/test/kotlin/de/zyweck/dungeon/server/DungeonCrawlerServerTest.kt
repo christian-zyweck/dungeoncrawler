0c99ddea71516d32934201d76b608fb687768966
Christian Zyweck <c.zyweck@gmail.com>
2024-08-09 20:25:10 +0200

    commits initial project files

---
+++ src/test/kotlin/de/zyweck/dungeon/server/DungeonCrawlerServerTest.kt
@@ -0,0 +1,13 @@
+package de.zyweck.dungeon.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DungeonCrawlerServerTest {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
